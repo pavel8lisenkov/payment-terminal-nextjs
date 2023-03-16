380ff461ce82f7f167664502a95928317c99f4c9
Pasha <pavel8lisenkov@yandex.ru>
2023-03-16 16:48:22 +0400

    edit size form

--- _next/static/DYdtlMFolBsD_jsMO9tgI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5d67477909d40976.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/operator/[id]":["static/chunks/pages/operator/[id]-e0c91a48dc4b8004.js"],sortedPages:["/","/_app","/_error","/operator/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/h0SCPVuS6G3_Op0Ueevym/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0821900b75cf595e.js"],"/_error":["static/chunks/pages/_error-b9cfc7093ea29d17.js"],"/operator/[id]":["static/chunks/pages/operator/[id]-9cba365af0228bb9.js"],sortedPages:["/","/_app","/_error","/operator/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
